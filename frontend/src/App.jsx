86f186d83aaa9ddc51fcb2fee9fe6e726850702b
abdullahnyle <abdullahch1715@icloud.com>
2026-05-13 02:12:07 +0500

    Render multiple FragranceCards via map over hardcoded array

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
 import FragranceCard from './FragranceCard'
 import './App.css'
 
-function App(){
-  return(
+function App() {
+  const fragrances = [
+    { name : "Sauvage",brand : "Dior", accords : "Citrus, Woody, Spicy" },
+    { name : "Bleu de Chanel", brand : "Chanel", accords : "Citrus, Woody, Aromatic" },
+    { name : "Kaaf", brand : "Ahmed Al Maghribi", accords : "Citrus, Woody, Aromatic" },
+  ]
+
+  return (
     <>
-    <h1>FragBro</h1>
-    <p>Catalog</p>
-    <FragranceCard frag={{ name: "Sauvage", brand: "Dior", accords: "fresh, ambroxan, citrus"}} />
- </>
+      <h1>FragBro</h1>
+      <p>Catalog</p>
+      {fragrances.map(frag => (
+        <FragranceCard key={frag.name} frag={frag} />
+      ))}
+    </>
   )
 }
- export default App
+
+export default App
